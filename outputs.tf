26badd8ca9c30d5bb6157775f2958dd2f471b9be
Syed Hassaan Ahmed <syah@microsoft.com>
2020-05-17 03:32:56 +0200

    Remove unnecessary output

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,3 @@
-output rg_name {
-  value = azurerm_resource_group.rg.name
-}
-
-output aks_cluster_name {
-  value = azurerm_kubernetes_cluster.aks.name
-}
-
 output ingress_fqdn {
   value = azurerm_public_ip.ingress.fqdn
 }
